5831514f0c49fd36c1b0f9104b1f36f8b22da6c6
Daniil_Suchkov <rewasors@gmail.com>
2018-09-14 01:36:38 +0300

    HW1,2

---
+++ Lesson1/page2.php
@@ -0,0 +1,3 @@
+<?php
+    header('location : /Lesson1/index.php');
+?>
---
+++ Lesson2/index.php
@@ -0,0 +1,71 @@
+<?php
+//Задание 1
+$a = 455;
+$b = 100;
+
+if ($a >= 0 && $b >= 0) {
+    echo($a - $b);
+    echo "<br>";
+}
+if ($a < 0 && $b < 0) {
+    echo($a*$b);
+    echo "<br>";
+}
+if ($a >= 0 && $b < 0 || $a <= 0 && $b >=0) {
+    echo($a+$b);
+    echo "<br>";
+}
+
+//Задание 3 
+//Сложение
+function summ($a, $b){
+    $result = $a + $b;
+    return $result;
+}
+//Разность
+function diff($a, $b){
+    $result = $a - $b;
+    return $result;
+}
+//Умножение
+function milti($a,$b){
+    $result = $a * $b;
+    return $result;
+}
+//Деление
+function divis($a, $b){
+    $result = $a / $b;
+    return $result;
+}
+//Задание 4
+function mathOperation($arg1, $arg2, $operation){
+    switch ($operation){
+        case summ:
+            echo(summ($arg1,$arg2));
+            break;
+        case diff:
+            echo(diff($arg1,$arg2));
+            break;
+        case milti:
+            echo(milti($arg1,$arg2));
+            break;
+        case divis:
+            echo(divis($arg1,$arg2));
+            break;
+    }
+}
+//Задание 6
+function power($val, $pow){
+    if ($pow > 0){
+        static $powResult;
+        $powResult = $val * $val;
+        $pow--;
+        var_dump($val);
+        var_dump($powResult);
+        power($val,$pow);
+    } return $powResult;
+}
+var_dump(power(2,4));
+//Задание 7 
+echo "<br>";
+echo date('h,m');
